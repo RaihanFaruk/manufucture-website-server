4f3de612c31667a0b66fd85ac0fde7e2e5e07a2b
Mehejabin <RaihanFaruk>
2022-05-27 22:36:46 +0600

    express mongo stripe jwt

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000;
