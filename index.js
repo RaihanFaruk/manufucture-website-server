ef8e179171127a16730ad5405ffb29ee15571bf9
Mehejabin <RaihanFaruk>
2022-05-27 22:39:05 +0600

    jwt add

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ const corsConfig = {
     origin: true,
     credentials: true,
 }
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+const emailSenderOptions = {
+    auth: {
+        api_key: process.env.EMAIL_SENDER_KEY
+    }
+}
+
